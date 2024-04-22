c953dff56ac6b64a1eed83cec28bcdc962df788a
Isabel Robleda <isabel.robleda@gmail.com>
2024-04-22 10:56:41 +0200

    addition of css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css'
-import RestaurantsList from './components/RestaurantsList'
+import "./App.css";
+import RestaurantsList from "./components/RestaurantsList";
 
 function App() {
-  
-
   return (
     <>
-      <h1>Restaurants Near You</h1>
-      <RestaurantsList />
+      <div className="flex flex-col items-center">
+        <h1 className="font-semibold text-xl p-8">Restaurants Near You</h1>
+        <RestaurantsList />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 const RestaurantCard = ({ restaurant }) => {
   return (
-    <div >
-      <h2>{restaurant.name}</h2>
-      <p>Cuisines: {restaurant.cuisines.join(', ')}</p>
-      <p>Rating: {restaurant.rating}</p>
-      <p>Address: {restaurant.address}</p>
+    <div className='border rounded-md mb-5 py-8 px-10 hover:bg-slate-100 hover:border-slate-100'>
+      <h2 className='font-semibold mb-1'>{restaurant.name}</h2>
+      {restaurant.cuisines.map((cuisine, index) => (
+          <span key={index} className='bg-slate-400 text-white rounded-full px-3 py-1 text-sm mr-1'>
+            {cuisine}
+          </span>
+        ))}
+      <p className='text-sm mt-1'>Rating: {restaurant.rating}</p>
+      <p className='text-sm'><b>Address:</b> {restaurant.address}</p>
     </div>
   );
 };
